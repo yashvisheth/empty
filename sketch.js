bd4d5a528c329e45595dfa78985b7e1530321b11
yashvisheth <2513.sheth.yashvi@gmail.com>
2021-05-09 11:39:56 +0530

    Add Platform

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
@@ -10,34 +10,34 @@ function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
+function setup() {
+    var canvas = createCanvas(1200, 400);
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
+    platform = new Ground(150, 300, 300, 170)
+    ground = new Ground(600, height, 1200, 20)
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
+    box1 = new Box(700, 320, 70, 70);
+    box2 = new Box(920, 320, 70, 70);
     pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    log1 = new Log(810, 260, 300, PI / 2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
+    box3 = new Box(700, 240, 70, 70);
+    box4 = new Box(920, 240, 70, 70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log3 = new Log(810, 180, 300, PI / 2);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    box5 = new Box(810, 160, 70, 70);
+    log4 = new Log(760, 120, 150, PI / 7);
+    log5 = new Log(870, 120, 150, -PI / 7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(100, 100);
 
 }
 
-function draw(){
+function draw() {
     background(backgroundImg);
     Engine.update(engine);
     console.log(box2.body.position.x);
@@ -57,6 +57,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    platform.display();
     bird.display();
 }
